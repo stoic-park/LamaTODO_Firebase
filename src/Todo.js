063edc41efcd8b1326f917934e03f6634a0749f3
stoic-park <demige79@gmail.com>
2020-09-03 22:39:16 +0900

    :wrench: Fix: add delete button
    - 삭제 버튼 추가
    - material-ui icons 사용

--- src/Todo.js
+++ src/Todo.js
@@ -7,7 +7,12 @@ import {
   ListItemAvatar,
   Avatar,
   ImageIcon,
+  Button,
 } from "@material-ui/core";
+import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+
+// firebase
+import db from "./firebase";
 
 function Todo(props) {
   return (
@@ -22,8 +27,14 @@ function Todo(props) {
           </Avatar> */}
         </ListItemAvatar>
 
-        <ListItemText primary={props.todo} secondary="마감 기한"></ListItemText>
+        <ListItemText
+          primary={props.todo.todo}
+          secondary="마감 기한"
+        ></ListItemText>
       </ListItem>
+      <DeleteForeverIcon
+        onClick={(event) => db.collection("todos").doc(props.todo.id).delete()}
+      />
     </List>
   );
 }
