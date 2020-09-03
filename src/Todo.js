6426ea26ffaf1a757e5ab349c285b8848fd2ba89
stoic-park <demige79@gmail.com>
2020-09-03 23:16:48 +0900

    :wrench: Fix: add update data
    - 모달창을 활용한 기존 데이터 수정 기능 추가
    - firebase set
    - 리얼타임 db에 접근이 쉽기 때문에 데이터 수정도 훨씬 간단하다!

--- src/Todo.js
+++ src/Todo.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Todo.css";
 import {
   List,
@@ -8,34 +8,86 @@ import {
   Avatar,
   ImageIcon,
   Button,
+  Modal,
+  Input,
 } from "@material-ui/core";
 import DeleteForeverIcon from "@material-ui/icons/DeleteForever";
+import { makeStyles } from "@material-ui/core/styles";
 
 // firebase
 import db from "./firebase";
 
+const useStyles = makeStyles((theme) => ({
+  paper: {
+    position: "absolute",
+    width: 400,
+    backgroundColor: theme.palette.background.paper,
+    border: "2px solid #000",
+    boxShadow: theme.shadows[5],
+    padding: theme.spacing(2, 4, 3),
+  },
+}));
+
 function Todo(props) {
+  // hooks & Modal 창을 사용해서 수정기능 추가하기
+  const [open, setOpen] = useState(false);
+  const [input, setInput] = useState();
+
+  // Material -> styles
+  const classes = useStyles();
+
+  const handleOpen = () => {
+    setOpen(true);
+  };
+
+  // 수정 함수
+  const updateTodo = () => {
+    // 새로운 입력 값으로 업데이트
+    db.collection("todos").doc(props.todo.id).set(
+      {
+        todo: input,
+      },
+      { merge: true }
+    );
+    setOpen(false);
+  };
+
   return (
-    // <div>
-    //   <li>{props.todo}</li>
-    // </div>
-    <List className="todolist">
-      <ListItem>
-        <ListItemAvatar>
-          {/* <Avatar>
+    <>
+      <Modal open={open} onClose={(e) => setOpen(false)}>
+        <div className={classes.paper}>
+          <h1>fix it!</h1>
+          <Input
+            // 기존 값도 나타내주자
+            placeholder={props.todo.todo}
+            value={input}
+            onChange={(event) => setInput(event.target.value)}
+          />
+          <Button onClick={(e) => updateTodo()}>update</Button>
+        </div>
+      </Modal>
+
+      <List className="todolist">
+        <ListItem>
+          <ListItemAvatar>
+            {/* <Avatar>
             <ImageIcon />
           </Avatar> */}
-        </ListItemAvatar>
+          </ListItemAvatar>
 
-        <ListItemText
-          primary={props.todo.todo}
-          secondary="마감 기한"
-        ></ListItemText>
-      </ListItem>
-      <DeleteForeverIcon
-        onClick={(event) => db.collection("todos").doc(props.todo.id).delete()}
-      />
-    </List>
+          <ListItemText
+            primary={props.todo.todo}
+            // secondary="마감 기한"
+          ></ListItemText>
+        </ListItem>
+        <Button onClick={(e) => setOpen(true)}>수정</Button>
+        <DeleteForeverIcon
+          onClick={(event) =>
+            db.collection("todos").doc(props.todo.id).delete()
+          }
+        />
+      </List>
+    </>
   );
 }
 
