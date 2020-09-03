6ecab2c69d0bd2e57b1011aabadbfc6dd637043c
stoic-park <demige79@gmail.com>
2020-09-03 20:29:25 +0900

    :fire: Fix: App.js detail fix
    - firebase db 불러오기
    - 데이터 추가하기

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./App.css";
-
+import Todo from "./Todo";
 //
 // import Button from "@material-ui/core/Button";
 import { Button, FormControl, InputLabel, Input } from "@material-ui/core";
 
+// import firebase db
+import db from "./firebase";
+import firebase from "firebase";
+
 function App() {
   // state
-  const [todos, setTodos] = useState(["알고리즘 풀기", "책 읽기"]);
-  const [input, setInput] = useState("");
+  // const [todos, setTodos] = useState(["알고리즘 풀기", "책 읽기"]);
+  // const [input, setInput] = useState("");
   // console.log(input);
   // console.log(todos);
+
+  //! firebase store
+  const [todos, setTodos] = useState([]);
+  const [input, setInput] = useState("");
+
+  // when the app loads, we need to listen to the database and fetch new todos as they get added/removed
+  useEffect(() => {
+    // 여기에 코드..! app.js 가 불러와졌을 때 실행될 코드
+    //! firestore에서 데이터 가져오기!
+    //! collection - onSnapshot!
+    // orderBy이용해서 내림차순 정렬하기
+    db.collection("todos")
+      .orderBy("timestamp", "desc")
+      .onSnapshot((snapshot) => {
+        // console.log(snapshot.docs.map((doc) => doc.data().todo));
+        setTodos(snapshot.docs.map((doc) => doc.data().todo));
+      });
+  }, []);
 
   // 버튼 클릭시 todos에 state 추가
   const addTodo = (event) => {
@@ -22,8 +44,17 @@ function App() {
     //! will stop the refresh
     event.preventDefault();
 
+    //! firestore에 데이터 추가하기!
+    //! collection - add
+    // very simple code..!
+    db.collection("todos").add({
+      todo: input,
+      // 시간
+      timestamp: firebase.firestore.FieldValue.serverTimestamp(),
+    });
+
     // setTodos(input); // =>  error 발생!!
-    setTodos([...todos, input]);
+    // setTodos([...todos, input]);
     setInput("");
     // console.log(todos);
   };
@@ -66,7 +97,8 @@ function App() {
       <ul>
         {/* map 사용해서 todo 뿌리기 */}
         {todos.map((todo) => (
-          <li>{todo}</li>
+          // <li>{todo}</li>
+          <Todo todo={todo} />
         ))}
       </ul>
     </div>
